3776d94d3d26c6d3a456a758407a5cf2e3dee65c
Yb Arzoo <87442336+YbArzoo@users.noreply.github.com>
2025-05-08 02:50:45 +0600

    render e hobe to?

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///hashmurgi.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'sX!7uR#eP9@zLmT8^c3Gb$NqWx&V5jYp')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///hashmurgi.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join('static', 'uploads')
-
